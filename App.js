4ac3b4c56ad1ff467527b9d1e88a70fb4afefc9d
melanie <mel.ep.morin@gmail.com>
2018-12-17 16:51:00 +0100

    escape logo de splash screen

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-      <Image resizeMode="contain" source={require ("./assets/escape.png")}></Image>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Image resizeMode="contain" style={{flexShrink:1}} source={require ("./assets/escape.png")}></Image>
       </View>
     );
   }
