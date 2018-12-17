38b0edce8fe4a2e2f9a5a6ca8ec8f8016534c493
melanie <mel.ep.morin@gmail.com>
2018-12-17 16:53:20 +0100

    bouton start

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
       <Image resizeMode="contain" style={{flexShrink:1}} source={require ("./assets/escape.png")}></Image>
+      <TouchableOpacity><Text>Toucher pour commencer</Text></TouchableOpacity>
       </View>
     );
   }
