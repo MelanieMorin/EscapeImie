392c5b7beae479b94507fc36ef11e0fd2231b790
melanie <mel.ep.morin@gmail.com>
2018-12-17 16:28:30 +0100

    escape png

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Image resizeMode='contain' style={{flexShrink:1}} source={require ("./assets/escape.png")}></Image>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
@@ -14,8 +15,9 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundSize:contain,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
