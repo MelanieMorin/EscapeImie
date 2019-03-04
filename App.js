aa398a6d68325ad8933b28e6058976774af8fbf2
melanie <mel.ep.morin@gmail.com>
2019-03-04 10:44:08 +0100

    qq modifs

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TouchableHighlight  } from 'react-native';
+import { GameEngine } from "react-native-game-engine";
+import { Finger } from "./renderers";
+import { MoveFinger } from "./systems"
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +11,20 @@ export default class App extends React.Component {
       <Image resizeMode="contain" style={{flexShrink:1}} source={require ("./assets/escape.png")}></Image>
       <TouchableOpacity><Text>Toucher pour commencer</Text></TouchableOpacity>
       </View>
+      <GameEngine
+        style={styles.container}
+        systems={[MoveFinger]}
+        entities={{
+          1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
+          2: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
+          3: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
+          4: { position: [220, 200], renderer: <Finger />},
+          5: { position: [280, 200], renderer: <Finger />}
+        }}>
+
+        <StatusBar hidden={true} />
+
+      </GameEngine>
     );
   }
 }
